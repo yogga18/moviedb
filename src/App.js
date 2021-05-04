d922047d6e15bf497fec7854f69a2954a7c5e7c9
yogga18 <yogga910@gmail.com>
2021-05-04 12:51:34 +0700

    added detail feature film and added react-routing useHistory FINISH

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import DetailMovies from './pages/DetailMovies';
 import ScrollableTabsButtonAuto from './pages/One';
+
 
 function App() {
   return (
-    <ScrollableTabsButtonAuto/>
+    <Router>
+      <Fragment>
+
+        <Switch>
+          <Route path="/" exact component={ScrollableTabsButtonAuto}/>
+          <Route path="/DetailMovies/:id" component={DetailMovies} />
+        </Switch>
+
+      </Fragment>
+    </Router>
   )
 }
 
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,17 +1,17 @@
 import React, {useState, useEffect} from 'react';
-
+import {useHistory} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 import config_axios from './config_axios';
-
-import './pages.css'
-
+import './pages.css';
 
 
 
-const base_img = 'https://image.tmdb.org/t/p/w300/'
+
+const base_img = 'https://image.tmdb.org/t/p/w300/';
 
 const Movie = (props) => {
     const [movies, setMovies] = useState([]);
-
+    const history = useHistory();
     
 
     
@@ -29,25 +29,31 @@ const Movie = (props) => {
     return (
         <div className="row">
                 {
-                    movies.map((data) => {
+                    movies.map((movies) => {
                         return(
                             <div className="card">
                                 <div>
-                                    <img src={`${base_img}${data.poster_path}`}
+                                    <img src={`${base_img}${movies.poster_path}`}
                                         className="card__image"
-                                        alt={data.original_title}
-                                        key={data.id} /> 
+                                        alt={movies.original_title}
+                                        key={movies.id} /> 
                                 </div>
 
                                 <div className="card__content">         
-                                    <h1 className="card__title">{data.title}{data.original_name}</h1>
+                                    <h1 className="card__title">{movies.title}{movies.original_name}</h1>
                                     <hr/>
                                     <h2>Overview</h2>
-                                    <div className="card__desc">{data.overview}</div>
-                                    <h4 className="card__date">Release Date : {data.release_date}</h4>
-                                    <h4 className="card__rating">Rating {data.vote_average}</h4>
-                                </div>
+                                    <div className="card__desc">{movies.overview}</div>
+                                    <h4 className="card__date">Release Date : {movies.release_date}</h4>
+                                    <h4 className="card__rating">Rating {movies.vote_average}</h4>
+                                    <hr/>
                                     
+                                    <Button
+                                    variant="contained"
+                                    color="primary"
+                                    movies={movies}
+                                    onClick={() => history.push(`/DetailMovies/${movies.id}`)}>Detail</Button>
+                                </div>
 
                             </div>
                         )
