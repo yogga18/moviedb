d653afeb283e83a5c3e5f5fb53f0787f5fa71485
Yogga Aditya Candra <yogga910@gmail.com>
2023-03-02 17:46:41 +0700

    wake up

--- src/pages/DetailMovies.jsx
+++ src/pages/DetailMovies.jsx
@@ -1,80 +1,91 @@
-import React, {useEffect, useState, Fragment} from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import './DetailMovies.css';
 import axios from 'axios';
-
 
 const base_img = 'https://image.tmdb.org/t/p/w500/';
 const baseUrl = 'https://api.themoviedb.org/3/movie/';
 const apiKey = 'ba57158e113b297b193e71fffbecfb62';
 
 function DetailMovies(props) {
-    const [detail, setDetail] = useState([]);
+  const [detail, setDetail] = useState([]);
 
-    useEffect(() => {
-        const fetchDetail = async () => {
-            const id = props.match.params.id
-            const request = await axios.get(`${baseUrl}${id}?api_key=${apiKey}`)
-            .then((result) => {
-                setDetail([result.data])
-                console.log(result.data)
-            }).catch((err) => {
-                console.log(err)
-            });
-        }
+  useEffect(() => {
+    const fetchDetail = async () => {
+      const id = props.match.params.id;
+      const request = await axios
+        .get(`${baseUrl}${id}?api_key=${apiKey}`)
+        .then((result) => {
+          setDetail([result.data]);
+          console.log(result.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
 
-        fetchDetail();
-    }, [])
+    fetchDetail();
+  });
 
-    // console.log(props.match.params)
-    return (
-        <Fragment>
-            {
-                detail.map((detail) => {
-                    return(
-                        <div className="detai-wrapper">
-                                    
-                                <div className="poster-wrapper">
-                                    <div className="Wrap-img">
-                                        <img src={`${base_img}${detail.poster_path}`}
-                                            className="backdrop_path"
-                                            alt={detail.original_title}
-                                            key={detail.id} />
-                                    </div>
-                                    <div className="Wrap-desc">
-                                        <h1 className="title-movies">{detail.title}</h1>
-                                        <p className="tag-line"><i>" {detail.tagline} "</i></p>
-                                        <h1>Overview</h1>
-                                        <p>{detail.overview}</p>
-                                        <ul>
-                                            <li><p>Release Date : {detail.release_date}</p></li>
-                                            <li><p>Status {detail.status}</p></li>
-                                            <li><p>Budget Production {detail.budget}</p></li>
-                                            <li><p>Popularity {detail.popularity}</p></li>
-                                            <li><p>Rating {detail.vote_average}</p></li>
-                                        </ul>
-                                        {/* <p>Release Date : {detail.release_date}</p>
+  // console.log(props.match.params)
+  return (
+    <Fragment>
+      {detail.map((detail) => {
+        return (
+          <div className='detai-wrapper'>
+            <div className='poster-wrapper'>
+              <div className='Wrap-img'>
+                <img
+                  src={`${base_img}${detail.poster_path}`}
+                  className='backdrop_path'
+                  alt={detail.original_title}
+                  key={detail.id}
+                />
+              </div>
+              <div className='Wrap-desc'>
+                <h1 className='title-movies'>{detail.title}</h1>
+                <p className='tag-line'>
+                  <i>" {detail.tagline} "</i>
+                </p>
+                <h1>Overview</h1>
+                <p>{detail.overview}</p>
+                <ul>
+                  <li>
+                    <p>Release Date : {detail.release_date}</p>
+                  </li>
+                  <li>
+                    <p>Status {detail.status}</p>
+                  </li>
+                  <li>
+                    <p>Budget Production {detail.budget}</p>
+                  </li>
+                  <li>
+                    <p>Popularity {detail.popularity}</p>
+                  </li>
+                  <li>
+                    <p>Rating {detail.vote_average}</p>
+                  </li>
+                </ul>
+                {/* <p>Release Date : {detail.release_date}</p>
                                         <p>Status {detail.status}</p>
                                         <p>Budget Production {detail.budget}</p>
                                         <p>Popularity {detail.popularity}</p>
                                         <p>Rating {detail.vote_average}</p> */}
 
-                                        <div className="second-img">
-                                            <img src={`${base_img}${detail.backdrop_path}`}
-                                                    className="second-poster"
-                                                    alt={detail.original_title}
-                                                    key={detail.id} />
-                                        </div>
-                                    </div>
-
-                                </div>
-                           
-
-                        </div>
-                    )
-                })
-            }
-        </Fragment>
-    )
+                <div className='second-img'>
+                  <img
+                    src={`${base_img}${detail.backdrop_path}`}
+                    className='second-poster'
+                    alt={detail.original_title}
+                    key={detail.id}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </Fragment>
+  );
 }
 
-export default DetailMovies
+export default DetailMovies;
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,66 +1,66 @@
-import React, {useState, useEffect} from 'react';
-import {useHistory} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import config_axios from './config_axios';
 import './pages.css';
 
-
-
-
 const base_img = 'https://image.tmdb.org/t/p/w300/';
 
 const Movie = (props) => {
-    const [movies, setMovies] = useState([]);
-    const history = useHistory();
-    
+  const [movies, setMovies] = useState([]);
+  const history = useHistory();
 
-    
-    useEffect(() => {
-        const fetchMovies = async () => {
-            const request = await config_axios.get(props.url)
-            setMovies(request.data.results)
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const request = await config_axios.get(props.url);
+      setMovies(request.data.results);
+    };
 
+    fetchMovies();
+  });
 
-        }
+  return (
+    <div className='row'>
+      {movies.map((movies) => {
+        return (
+          <div className='card'>
+            <div>
+              <img
+                src={`${base_img}${movies.poster_path}`}
+                className='card__image'
+                alt={movies.original_title}
+                key={movies.id}
+              />
+            </div>
 
-        fetchMovies();
-    }, [])
+            <div className='card__content'>
+              <h1 className='card__title'>
+                {movies.title}
+                {movies.original_name}
+              </h1>
+              <hr />
+              <h2>Overview</h2>
+              <div className='card__desc'>{movies.overview}</div>
+              <h4 className='card__date'>
+                Release Date : {movies.release_date}
+              </h4>
+              <h4 className='card__rating'>Rating {movies.vote_average}</h4>
+              <hr />
 
-    return (
-        <div className="row">
-                {
-                    movies.map((movies) => {
-                        return(
-                            <div className="card">
-                                <div>
-                                    <img src={`${base_img}${movies.poster_path}`}
-                                        className="card__image"
-                                        alt={movies.original_title}
-                                        key={movies.id} /> 
-                                </div>
-
-                                <div className="card__content">         
-                                    <h1 className="card__title">{movies.title}{movies.original_name}</h1>
-                                    <hr/>
-                                    <h2>Overview</h2>
-                                    <div className="card__desc">{movies.overview}</div>
-                                    <h4 className="card__date">Release Date : {movies.release_date}</h4>
-                                    <h4 className="card__rating">Rating {movies.vote_average}</h4>
-                                    <hr/>
-                                    
-                                    <Button
-                                    variant="contained"
-                                    color="primary"
-                                    movies={movies}
-                                    onClick={() => history.push(`/DetailMovies/${movies.id}`)}>Detail</Button>
-                                </div>
-
-                            </div>
-                        )
-                    })
-                }
-        </div>
-    )
-}
+              <Button
+                variant='contained'
+                color='primary'
+                movies={movies}
+                onClick={() => history.push(`/DetailMovies/${movies.id}`)}
+              >
+                Detail
+              </Button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Movie;
